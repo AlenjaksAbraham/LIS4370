348f2f0429226d249de6e4fcca40ff886de0083a
Alen Abraham <alenmicro@outlook.com>
2025-02-16 00:38:37 -0500

    Module 5

---
+++ Module 5.R
@@ -0,0 +1,19 @@
+#Your Assignment:
+  #Find the value of inverse of a matrix, determinant of a matrix by using the following values:
+  #A=matrix(1:100, nrow=10)
+  #B=matrix(1:1000, nrow=10)
+
+
+A <- matrix(1:100, nrow=10)  
+B <- matrix(1:1000, nrow=10)
+
+
+A
+B
+
+
+det(A)
+det(B)
+
+dim(A)
+dim(B)
